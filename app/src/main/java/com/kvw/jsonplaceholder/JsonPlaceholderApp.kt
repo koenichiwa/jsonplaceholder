108371191dc0a2a8dec29e69bf7a74abd574ed8d
Koen <koenvwel@gmail.com>
2020-01-22 00:24:55 +0100

    Set up DI and Logging

---
+++ app/src/main/java/com/kvw/jsonplaceholder/JsonPlaceholderApp.kt
@@ -0,0 +1,26 @@
+package com.kvw.jsonplaceholder
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+class JsonPlaceholderApp : Application(){
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@JsonPlaceholderApp)
+            modules(
+                listOf(
+                    KoinModules.retrofitModule,
+                    KoinModules.repositoryModule
+                )
+            )
+        }
+
+        if (BuildConfig.DEBUG) { Timber.plant(Timber.DebugTree()) }
+    }
+}
---
+++ app/src/main/java/com/kvw/jsonplaceholder/KoinModules.kt
@@ -0,0 +1,33 @@
+package com.kvw.jsonplaceholder
+
+import com.google.gson.GsonBuilder
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object KoinModules {
+    private const val JSON_PLACEHOLDER_BASE_URL = "https://jsonplaceholder.typicode.com"
+
+    val repositoryModule = module {
+        factory<UserRepository> { UserRepositoryDefault(get()) }
+    }
+
+    val retrofitModule = module {
+        single { provideRetrofit(provideGsonConverterFactory()) }
+        factory<UserService> { get<Retrofit>().create(UserService::class.java) }
+    }
+
+    private fun provideGsonConverterFactory(): GsonConverterFactory {
+        GsonBuilder()
+            .create()
+            .let { return GsonConverterFactory.create(it) }
+    }
+
+    private fun provideRetrofit(converterFactory: GsonConverterFactory): Retrofit {
+        return Retrofit
+            .Builder()
+            .addConverterFactory(converterFactory)
+            .baseUrl(JSON_PLACEHOLDER_BASE_URL)
+            .build()
+    }
+}
