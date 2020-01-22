193683fbce2474498d197fe1989c1039ed17b905
Koen <koenvwel@gmail.com>
2020-01-23 00:43:26 +0100

    Change Intel

--- app/src/main/java/com/kvw/jsonplaceholder/util/Intel.kt
+++ app/src/main/java/com/kvw/jsonplaceholder/util/Intel.kt
@@ -1,5 +1,11 @@
 package com.kvw.jsonplaceholder.util
 
+import android.view.View
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import timber.log.Timber
+
 sealed class Intel<T>(val source: Source) {
     class Pending<T> : Intel<T>(Source.Local)
     class Success<T>(source: Source, val data: T) : Intel<T>(source)
@@ -10,3 +16,40 @@ sealed class Intel<T>(val source: Source) {
     }
 }
 
+fun <T> LiveData<Intel<T>>.observe(owner: LifecycleOwner,
+                                   onPending: () -> Unit = {},
+                                   onSuccess: (T)->Unit,
+                                   onError: (Throwable, String) -> Unit) {
+    this.observe(owner, Observer{
+        when(it){
+            is Intel.Pending -> {
+                onPending()
+                Timber.d("Waiting for intel")
+            }
+            is Intel.Success -> {
+                Timber.d("Intel received")
+                onSuccess(it.data)
+            }
+            is Intel.Error -> {
+                Timber.d(it.throwable, "Intel collection of threw an error, reason: ${it.reason}")
+                onError(it.throwable, it.reason)
+            }
+        }
+    })
+}
+
+fun <T> LiveData<Intel<T>>.observe(owner: LifecycleOwner,
+                                          pendingView: View?,
+                                          onSuccess: (T)->Unit,
+                                          onError: (Throwable, String) -> Unit){
+    this.observe(owner,
+        onPending = {
+            pendingView?.visibility = View.VISIBLE
+        },
+        onSuccess = {
+            pendingView?.visibility = View.GONE
+            onSuccess(it)
+        },
+        onError = onError
+    )
+}
