0af8a06610e375dc0a4760ce2d4c069281ca7f5c
Koen <koenvwel@gmail.com>
2020-01-22 01:37:22 +0100

    Update ViewModel

--- app/src/main/java/com/kvw/jsonplaceholder/ui/UserListViewModel.kt
+++ app/src/main/java/com/kvw/jsonplaceholder/ui/UserListViewModel.kt
@@ -3,9 +3,22 @@ package com.kvw.jsonplaceholder.ui
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kvw.jsonplaceholder.business.model.User
+import com.kvw.jsonplaceholder.business.repository.UserRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class UserListViewModel : ViewModel() {
-    private val _users = MutableLiveData<List<User>>(emptyList())
+@ExperimentalCoroutinesApi
+class UserListViewModel(userRepository: UserRepository) : ViewModel() {
+    private val _users = MutableLiveData<List<User>>()
     val users : LiveData<List<User>> get() = _users
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            userRepository.getAll().collect{_users.postValue(it) }
+        }
+    }
 }
